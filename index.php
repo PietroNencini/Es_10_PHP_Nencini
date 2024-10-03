62ff878052ef255904afd02c3eda9b865e6444d7
Pietro Nencini <nencinipietro2006@gmail.com>
2024-10-03 09:21:38 +0200

    Improved clean code

--- index.php
+++ index.php
@@ -13,12 +13,14 @@
         $num = rand(2,10);
         $par_text = "Hello World";
 
+        $final_text = "";
+
         for($i = 0; $i < $num; $i++) {
-            echo "<p style='font-size: ".$size."px'> $par_text </p>";
+            $final_text = $final_text."<p style='font-size: ".$size."px'> $par_text </p>";
             $size += 2;
         }
 
-
+        echo $final_text;
     ?>
 
 </body>
